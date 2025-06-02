abd724eacb5cfa90595b5e327a432384ce4a7555
Samedaysteamer <samedaysteamerz@gmail.com>
2025-06-01 22:06:08 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BookingLinkGenerator from './BookingLinkGenerator'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BookingLinkGenerator />
-  </React.StrictMode>
-)
