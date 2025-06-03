bb03397c40cdf79ff124046ebb880df4386a9013
Samedaysteamer <samedaysteamerz@gmail.com>
2025-06-03 19:09:15 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -17,13 +17,8 @@ Card payment: 7% processing fee
 Please fill out all information so we can create your work order and secure your time frame.`;
 
     const baseUrl = 'https://form.jotform.com/251536451249054';
-    const params = new URLSearchParams();
-    params.append('serviceType', serviceType);
-    params.append('quotedPrice', quotedPrice);
-    params.append('arrivalWindow', arrivalWindow);
-    params.append('bookingSummary', summary); // no encoding here!
-
-    const fullLink = `${baseUrl}?${params.toString()}`;
+    const encodedSummary = encodeURIComponent(summary);
+    const fullLink = `${baseUrl}?bookingSummary=${encodedSummary}`;
     setGeneratedLink(fullLink);
   };
 
