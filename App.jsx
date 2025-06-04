f85596c505d8cbcc16dded92f7c7c24cc749505c
Samedaysteamer <samedaysteamerz@gmail.com>
2025-06-03 20:05:19 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -12,19 +12,10 @@ export default function App() {
 $${quotedPrice} Special
 Arrival between ${arrivalWindow}
 Payment method: Cash Cashapp Zelle
-Card payment: 7% processing fee
-
-Please fill out all information so we can create your work order and secure your time frame.`;
+Card payment: 7% processing fee`;
 
     const baseUrl = 'https://form.jotform.com/251536451249054';
-
-    const params = new URLSearchParams();
-    params.append('serviceType', serviceType);
-    params.append('quotedPrice', quotedPrice);
-    params.append('arrivalWindow', arrivalWindow);
-    params.append('bookingSummary', encodeURIComponent(summary));
-
-    const fullLink = `${baseUrl}?${params.toString()}`;
+    const fullLink = `${baseUrl}?bookingSummary=${encodeURIComponent(summary)}`;
     setGeneratedLink(fullLink);
   };
 
