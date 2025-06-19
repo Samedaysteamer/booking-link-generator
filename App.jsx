8915463c9ab91bf14c1615b9cfc9c8ae507f6fe5
Samedaysteamer <samedaysteamerz@gmail.com>
2025-06-19 10:21:52 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -186,8 +186,8 @@ CashApp payment $5 fee
             <label>Duration of First Block (hrs):</label>
             <select value={blockHours} onChange={(e) => setBlockHours(e.target.value)}>
               <option value="2">2</option>
-              <option value="2">3</option>
-              <option value="3">1</option>
+              <option value="3">3</option>
+              <option value="1">1</option>
             </select>
           </div>
 
