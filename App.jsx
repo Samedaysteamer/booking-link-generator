c7b7d4125956c6ca172e1bb1dec9ca45007d3702
Samedaysteamer <samedaysteamerz@gmail.com>
2026-03-17 11:56:29 -0400

    Update App.jsx add *04*

--- App.jsx
+++ App.jsx
@@ -219,7 +219,7 @@ CashApp payment $5 fee
             <option value="*01*">*01*</option>
             <option value="*02*">*02*</option>
             <option value="*03*">*03*</option>
-            <option value="*04*">*03*</option>
+            <option value="*04*">*04*</option>
           </select>
         </div>
 
